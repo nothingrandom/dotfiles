66d23295462203ab962be4aeead63d2f2f7850b6
Benjamin Hollway <benjamin@nothingrandom.com>
2017-12-03 18:09:02 +0100

    Setup zsh-syntax-highlighting

--- node/install.sh
+++ node/install.sh
@@ -2,10 +2,3 @@ if test ! $(which spoof)
 then
   yarn global install spoof
 fi
-
-if test ! $(which pure-prompt)
-then
-  yarn global install pure-prompt
-  autoload -U promptinit; promptinit
-  prompt pure
-fi
