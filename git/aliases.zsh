eead684d57426c698aab0c22f2f264c2febb9349
Benjamin Hollway <benjamin@nothingrandom.com>
2019-10-17 13:14:18 +0200

    Remove git tree alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -13,7 +13,6 @@ alias gp='git push origin HEAD'
 
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 
-alias gtree='git tree' # config alias
 alias glog='git log --graph --pretty=format:"%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset" --abbrev-commit --date=relative'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
