fb2e46dd64cbc64bcf2a42987b376e1f8ff52b55
Benjamin Hollway <benjamin@nothingrandom.com>
2019-10-23 14:55:53 +0200

    .gitignore fix

--- code-portable-data.symlink/extensions/mrmlnc.vscode-scss-0.6.2/out/utils/document.d.ts
+++
@@ -1,9 +0,0 @@
-import { ISymbols } from '../types/symbols';
-/**
- * Returns imports for document.
- */
-export declare function getCurrentDocumentImportPaths(symbolsList: ISymbols[], currentPath: string): string[];
-/**
- * Returns the path to the document, relative to the current document.
- */
-export declare function getDocumentPath(currentPath: string, symbolsPath: string): string;
