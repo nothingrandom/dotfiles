63f36d7cc583237ee5db6fbb27c4a496cf9e4bcd
Benjamin Hollway <benjamin@nothingrandom.com>
2019-10-24 16:03:36 +0200

    Add zsh-autosuggestions

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,3 +4,8 @@ if [ ! -d "$HOME/.zsh-syntax-highlighting/" ];
 then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 fi
+
+if [ ! -d "$HOME/.zsh-autosuggestions/" ];
+then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh-autosuggestions
+fi
